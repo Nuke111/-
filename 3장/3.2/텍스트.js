a1cacfa434e1291793bc2082afbf8853242789f9
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-02-24 18:03:23 +0900

    Update 텍스트.js

--- 3장/3.2/텍스트.js
+++ 3장/3.2/텍스트.js
@@ -14,3 +14,17 @@ s.toUpperCase()         // HELLO WORLD
 //ECMAScript 5
 s[0]            // => "h"
 s[s.length-1]   // => "d"
+
+/*
+/^HTML/				// HTML로 시작하는 문자열
+/[1-9][0-9*]/		// 0 아닌 숫자로 시작하는 문자
+/\BJAVASCRIPT\b/i	// 대소문자 구별 없이 javascript와 일치하는 문자열
+*/
+
+var text = "testing: 1, 2, 3";
+var pattern = /\d+/g        // 하나 이상의 모든 숫자 일치
+pattern.test(text)          // => true: 일치하는 문자열이 존재
+text.search(pattern)        // => 9: 첫 번째로 매치하는 문자열의 위치
+text.match(pattern)         // => ["1", "2", "3"]: 일치된 항목의 배열
+text.replace(pattern, "#")  // => "testring: #, #, #"
+text.split(/\D+/);          // => ["", "1", "2", "3"]: 숫자가 아닌 문자(열)을 기준으로 분할
