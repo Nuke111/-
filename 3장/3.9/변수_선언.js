a7ac2d4bc13dea336c2c436ef701a53693a1afd4
hyeonuk-kor <mrseos@naver.com>
2021-07-07 16:04:18 +0900

    Update 변수_선언.js

--- 3장/3.9/변수_선언.js
+++ 3장/3.9/변수_선언.js
@@ -1,1 +1,14 @@
-//변수 선언
+// 변수 선언
+var i;
+var sum;
+
+// var 키워드를 이용하여 여러 개의 변수 선언
+var j, scores
+
+// 선언과 동시에 초기화
+var message = "hello";
+var k=0;
+
+// 아래 코드는 문법적으로 문제되지 않는다.
+var i = 10;
+i = "ten";
